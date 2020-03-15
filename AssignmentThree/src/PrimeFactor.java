85e5c75d0bd0ccdc7512554fd8c93a68a32ddb67
sune-1 <suga18ab@student.cbs.dk>
2020-03-15 23:40:39 +0100

    ass1: rewritten

--- AssignmentThree/src/PrimeFactor.java
+++ AssignmentThree/src/PrimeFactor.java
@@ -3,35 +3,42 @@ import java.util.Scanner;
 public class PrimeFactor {
 	
 	public static void main(String[] args) {
-		
-		long enteredNumber = readLong();
-			if (enteredNumber <= 1) {
-				System.out.println("Not a valid number");
-			}
-			convertNumber(enteredNumber);
-			
+		runPrimFactor();
 	}
 	
-	private static long readLong() {
-		System.out.println("Enter a positive integer to convert: ");
+	private static void runPrimFactor() {
+		System.out.println("Enter the number of times you wish to run the Prim Factor programme: ");
 		Scanner console = new Scanner(System.in);
-		long number = console.nextLong();
-		return number;
+		long runNumber = console.nextLong();
+		
+		for(long i = 1; i <= runNumber; i++ ) {
+			System.out.println("Enter a positive integer to convert: ");
+			long number = console.nextLong();
+			
+			if (number > 1) {
+				calculatePrimeFactor(number);
+			}else if(number <= 1) {
+				System.out.println("Not a valid number - Try again!");
+				number = console.nextLong();
+				calculatePrimeFactor(number);
+			}else if (number == 0) {
+				break;
+			}else {
+			}	
+		}
 	}
 	
-	private static void convertNumber(long number) {
+	
+	private static void calculatePrimeFactor(long number) {
 		for(long i = 2; i < number; i++) {
 			while(number%i == 0) {
 				if (i*i > number) {
 					break;
 				}
-					
 				System.out.print(i + ", ");
 				number = number/i;
 			}
-			
 		}
-		System.out.print(number);
+		System.out.println(number);
 	}
-	
 }
